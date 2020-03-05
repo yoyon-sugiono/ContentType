e2442d5e9619dc8e67398a8270d9448f9fd34450
yoyon-sugiono <berbagipeluang@gmail.com>
2020-03-05 13:30:15 +0700

    add json files

--- content-type.php
+++ content-type.php
@@ -7,24 +7,19 @@ class ContentType {
   static $content_types;
   
   public function __construct(){
-    self::$content_types = [
-      "json"  => "application/json",
-      "pdf"   => "application/pdf",
-      "txt"   => "text/plain",
-      "xml"   => "application/xml",
-      "png"   => "image/png",
-      "jpg"   => "image/jpeg",
-      "zip"   => "application/zip",
-      "rar"   => "application/x-rar-compressed"
-    ];
+    self::$content_types = json_decode(
+      file_get_contents(
+        __DIR__ ."/content-types.json"
+      )
+    );
   }
   
   public function display($type, $content){
-    if(! isset(self::$content_types[$type])):
+    if(! isset(self::$content_types->{$type})):
       die('Invalid content type');      
     endif;
     
-    $header = self::$content_types[$type];
+    $header = self::$content_types->{$type};
     
     header("Content-Type:$header");
     echo ($content);
